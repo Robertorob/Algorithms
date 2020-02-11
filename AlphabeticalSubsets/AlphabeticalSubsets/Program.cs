2ca684843dab03a9a3ccd1de35aada5f4e4636ef
Dmitrii Petukhov (Akvelon INC) <v-dmpetu@microsoft.com>
2020-02-11 15:17:04 +0300

    Add AlphabeticalSubset project

---
+++ AlphabeticalSubsets/AlphabeticalSubsets/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace AlphabeticalSubsets
+{
+    class Program
+    {
+        private static int[] d;
+        private static int n;
+        private static int[] set;
+
+        static void Main(string[] args)
+        {
+            set = new int[3] { 3, 1, 2 };
+            n = 3;
+            Array.Sort(set);
+
+            d = new int[set.Length + 1];
+
+            Func(0, -1);
+        }
+
+        public static void Func(int j, int prev)
+        {
+            if(prev == -2)
+            {
+                for (int i = 0; i < j - 1; i++)
+                {
+                    Console.Write(d[i] + " ");
+                }
+                Console.WriteLine();
+                
+                return;
+            }
+
+            Func(j + 1, -2);
+
+            for (int i = prev + 1; i < n; i++)
+            {
+                d[j] = set[i];
+                Func(j + 1, i);
+            }
+        }
+    }
+}
---
+++ InterestingDrink/InterestingDrink/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace InterestingDrink
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] x = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(f => int.Parse(f)).ToArray();
+            int q = int.Parse(Console.ReadLine());
+
+            int[] m = new int[q];
+
+            Array.Sort(x);
+
+            for (int i = 0; i < q; i++)
+            {
+                m[i] = BinarySearch(x, int.Parse(Console.ReadLine()) );
+            }
+
+            for (int i = 0; i < q; i++)
+            {
+                Console.WriteLine(m[i]);
+            }
+
+            
+        }
+
+        public static int BinarySearch(int[] mass, int value)
+        {
+            int l = -1, r = mass.Length - 1;
+
+            if (mass[0] > value)
+                return 0;
+
+            if (mass[mass.Length - 1] <= value)
+                return mass.Length;
+
+            while (l < r + 1)
+            {
+                int m = (r + l) / 2;
+                if (mass[m] <= value && mass[m + 1] > value)
+                    return m + 1;
+
+                if (mass[m] < value)
+                    l = m;
+                else
+                    r = m;
+            }
+            return r + 1;
+        }
+    }
+}
