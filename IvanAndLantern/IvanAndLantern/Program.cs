1e9cef9e1ebd7985bbf393a8893910afe94e29c2
Dmitrii Petukhov (Akvelon INC) <v-dmpetu@microsoft.com>
2020-02-12 12:18:23 +0300

    Solved Ivan and Lanterns

--- IvanAndLantern/IvanAndLantern/Program.cs
+++ IvanAndLantern/IvanAndLantern/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace IvanAndLantern
 {
@@ -6,7 +7,38 @@ namespace IvanAndLantern
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string line1 = Console.ReadLine();
+            string[] mass1 = line1.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            int n = int.Parse(mass1[0]);
+            int l = int.Parse(mass1[1]);
+
+            
+            string line2 = Console.ReadLine();
+            double[] a = line2.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(f => double.Parse(f)).ToArray();
+
+            double d = 0;
+
+            Array.Sort(a);
+
+            if (a[0] != 0)
+                d = a[0];
+            if (a[n - 1] != l)
+                d = Max((int) d, l - a[n - 1]);
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                double radius = (a[i + 1] - a[i]) / 2;
+                if(radius > d)
+                {
+                    d = radius;
+                }
+            }
+            Console.WriteLine(d);
+        }
+
+        private static double Max(double x, double y)
+        {
+            return x > y ? x : y;
         }
     }
 }
