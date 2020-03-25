d9c275cd77e0fa87600e5b6b1a5a8ddd98dd830c
Dmitrii Petukhov (Akvelon INC) <v-dmpetu@microsoft.com>
2020-03-25 14:17:51 +0300

    Solved NextPermutation

---
+++ NextPermutation/NextPermutation/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace NextPermutation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] array = new int[7] { 1, 2, 3, 7, 6, 5, 4 };
+            solve2(array);
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+        }
+
+        static void swap(int[] ar, int i, int j)
+        {
+            var tmp = ar[i];
+            ar[i] = ar[j];
+            ar[j] = tmp;
+        }
+
+
+        static void solve2(int[] ar)
+        {
+            int n = ar.Length;
+            var j = n - 2;
+            while (j >= 0 && ar[j] > ar[j + 1])
+            {
+                j--;
+            }
+
+            if (j != -1)
+            {
+                var i = j + 1;
+                var k = ar[j];
+                while (i < n && ar[i] > k)
+                {
+                    i++;
+                };
+
+                swap(ar, j, i - 1);
+            }
+
+            Array.Reverse(ar, j + 1, n - j - 1);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -21,3 +21,13 @@ This repo contains all solved tasks from Algorithm course in Akvelon
 ## QueensPermutation
 How many permutation of 8 queens is there if they do not beat each other?
 
+## NextPermutation
+Дана перестановка, найти следующую
+
+Решение: 
+
+1. Идем справа налево
+1. Находим убывание
+1. Число, на котором убывание произошло, свапаем с числом, которое должно быть следующим
+1. Реверсим правый конец
+
