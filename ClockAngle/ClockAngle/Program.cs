d732f405cbd02f41b47dac697bceb58df19f160b
Dmitrii Petukhov (Akvelon INC) <v-dmpetu@microsoft.com>
2020-06-02 14:10:56 +0300

    Commit

---
+++ ClockAngle/ClockAngle/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ClockAngle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = 1;
+            int minutes = 56;
+
+            Console.WriteLine(GetAngle(hours, minutes));
+
+            Console.ReadKey();
+        }
+
+        public static double GetAngle(int hours, int minutes)
+        {
+            if(hours > 11 || minutes >= 60)
+            {
+                throw new FormatException("Wrong format");
+            }
+
+            double hourMinutes = (double)hours * 5 + ((double)minutes / 60) * 5;
+            double angleMinutes = Math.Abs(hourMinutes - minutes);
+            return (angleMinutes / 60) * 360;
+        }
+    }
+}
