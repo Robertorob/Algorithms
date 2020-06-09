5794853771b20fefc3f20e80aeb049d7755adae1
Dmitrii Petukhov (Akvelon INC) <v-dmpetu@microsoft.com>
2020-06-09 13:58:54 +0300

    Graph diameter

---
+++ Graphs/FarNodes/FarNodes/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FarNodes
+{
+    public class Node
+    {
+        public int Value { get; set; }
+        public List<int> Neibhs { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Node> g = new List<Node>
+            {
+                new Node
+                {
+                    Value = 1,
+                    Neibhs = new List<int>{2}
+                },
+                new Node
+                {
+                    Value = 2,
+                    Neibhs = new List<int>{1,3}
+                },
+                new Node
+                {
+                    Value = 3,
+                    Neibhs = new List<int>{2,4,5}
+                },
+                new Node
+                {
+                    Value = 4,
+                    Neibhs = new List<int>{3}
+                },
+                new Node
+                {
+                    Value = 5,
+                    Neibhs = new List<int>{3,6}
+                },
+                new Node
+                {
+                    Value = 6,
+                    Neibhs = new List<int>{5}
+                }
+            };
+
+            bool[] used = new bool[6];
+            int[] lengts = new int[6];
+
+            KeyValuePair<int, int> kv = dfs(g, 0, 0, used);
+
+            bool[] used2 = new bool[6];
+
+            KeyValuePair<int, int> kv2 = dfs(g, kv.Key, 0, used2);
+
+            Console.WriteLine($"indexMax:{kv.Key + 1}, indexMax2: {kv2.Key + 1}");
+            Console.ReadKey();
+        }
+
+        public static KeyValuePair<int, int> dfs(List<Node> g, int i, int length, bool[] used)
+        {
+            used[i] = true;
+
+            int max = length;
+            int index = i;
+
+            foreach (var item in g[i].Neibhs)
+            {
+                if (used[item - 1] == false)
+                {
+                    KeyValuePair<int, int> kv = dfs(g, item - 1, length + 1, used);
+                    if(kv.Value > max)
+                    {
+                        max = kv.Value;
+                        index = kv.Key;
+                    }
+                }
+            }
+
+            return new KeyValuePair<int, int>(index, max);
+        }
+    }
+}
---
+++ Graphs/TasksOrder(Graphs)/TasksOrder(Graphs)/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TasksOrder_Graphs_
+{
+    public class Task
+    {
+        public List<int> Neib { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Task> tasks = new List<Task>
+            {
+                new Task
+                {
+                    Neib = new List<int> { 1, 2 }
+                }
+            };
+        }
+    }
+}
