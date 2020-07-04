b6245d1894fd6a8b0ae7b73702d412f12e8af116
Robert Akhmetshin (Akvelon INC) <v-roakh@microsoft.com>
2020-07-04 16:14:56 +0300

    Add July test

---
+++ JulyTest/AnyaAndMinimization/AnyaAndMinimization/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            var line1 = Console.ReadLine().Split(new char[] { ' ' });
+            var line2 = Console.ReadLine();
+            long n = long.Parse(line1[0]);
+            long k = long.Parse(line1[1]);
+
+
+            if (k == 0)
+            {
+                Console.WriteLine(line2);
+                return;
+            }
+
+            if (n == 1 && k > 0)
+            {
+                Console.WriteLine("0");
+                return;
+            }
+            if (n == 1 && k == 0)
+            {
+                Console.WriteLine(line2);
+                return;
+            }
+
+
+            var num = line2.ToCharArray();
+            int[] result = new int[num.Length]; //Требуемый массив
+            for (int i = 0; i < num.Length; i++)
+                result[i] = int.Parse(num[i].ToString());
+
+            for (int i = 0; i < n; i++)
+            {
+                if (i == 0)
+                {
+                    if (result[i] != 1 && k > 0)
+                    {
+                        result[i] = 1;
+                        k--;
+                    }
+                }
+                else
+                {
+                    if (result[i] == 0)
+                        continue;
+                    if (k > 0 && result[i] > 0)
+                    {
+                        result[i] = 0;
+                        k--;
+                    }
+                }
+            }
+
+
+
+            StringBuilder str = new StringBuilder();
+            for (int i = 0; i < result.Length; i++)
+            {
+                str.Append(result[i]);
+            }
+
+
+
+            Console.WriteLine(str);
+        }
+    }
+}
---
+++ JulyTest/MishaAndHandleChange/MishaAndHandleChange/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int q = int.Parse(Console.ReadLine());
+
+            if (q == 1)
+            {
+                var asdf = Console.ReadLine();
+                Console.WriteLine("1");
+                Console.WriteLine(asdf);
+                return;
+            }
+
+            Dictionary<string, string> list = new Dictionary<string, string>();
+
+            for (int i = 0; i < q; i++)
+            {
+                string value;
+                var line = Console.ReadLine().Split(new char[] { ' ' });
+                if (list.TryGetValue(line[0], out value))
+                {
+                    list.Remove(line[0]);
+                    list.Add(line[1], value);
+                }
+                else
+                    list.Add(line[1], line[0]);
+            }
+
+            Console.WriteLine(list.Count);
+            foreach (var item in list)
+            {
+                Console.WriteLine(item.Value + " " + item.Key);
+            }
+
+
+        }
+    }
+}
