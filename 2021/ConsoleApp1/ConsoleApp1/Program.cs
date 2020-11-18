9ec054369bef90482fe6ca6718003758cd0c8569
Robert Akhmetshin (Akvelon INC) <v-roakh@microsoft.com>
2020-11-18 13:17:56 +0300

    Сжатие песен

--- 2021/ConsoleApp1/ConsoleApp1/Program.cs
+++ 2021/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Net.NetworkInformation;
 
 namespace ConsoleApp1
 {
@@ -6,7 +8,49 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var line1 = SplitString();
+
+            long n = long.Parse(line1[0]);
+            long m = long.Parse(line1[1]);
+
+            long[] a = new long[n];
+            long[] diffs = new long[n];
+
+            for (long i = 0; i < n; i++)
+            {
+                var line = SplitString();
+
+                a[i] = long.Parse(line[0]);
+                diffs[i] = a[i] - long.Parse(line[1]);
+            }
+
+            long sum = a.Sum();
+            Array.Sort(diffs);
+
+            if (sum <= m)
+            {
+                Console.WriteLine("0");
+                return;
+            }
+
+            for (long i = diffs.Length - 1; i >= 0; i--)
+            {
+                sum -= diffs[i];
+
+                if (sum <= m)
+                {
+                    Console.WriteLine(i);
+                    return;
+                }
+            }
+
+            Console.WriteLine("-1");
+
+        }
+
+        public static string[] SplitString()
+        {
+            return Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
         }
     }
 }
